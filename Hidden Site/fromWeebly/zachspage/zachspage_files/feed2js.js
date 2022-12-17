19d54be8ab549ef57c10a31b12cabcc24390129a
TheBensonBoy <zachizkool101@gmail.com>
2022-12-16 22:45:20 -0700

    Added independent CSS Pages, removed old pages

--- Hidden Site/fromWeebly/zachspage/zachspage_files/feed2js.js
+++
@@ -1,3 +0,0 @@
-
-document.write("<div style=\"text-align: center;\">	<div class=\"rss-box\">		<p class=\"rss-title\">			<a class=\"rss-title\" href=\"\" target=\"_blank\">							</a>		</p>		<ul class=\"rss-items\">		</ul>	</div></div><div style=\"clear: both; width: 0; height: 0; overflow: hidden;\"></div>");
-
